51e0583ed96907735bf9139297b288fd54d08ace
aramsadoyan <aram.sadoyan@picsart.com>
2024-02-07 23:05:50 +0400

    koin definition crash fix cleanup gradle for fb

--- app/src/main/java/com/ins/boostyou/composable/FirstPage.kt
+++ app/src/main/java/com/ins/boostyou/composable/FirstPage.kt
@@ -16,43 +16,53 @@ import com.ins.boostyou.viewModel.ComposeNavigationViewModel
 import com.ins.boostyou.viewModel.InAppPurchaseViewModel
 import com.ins.boostyou.viewModel.MainActivityViewModel
 
-@Preview
 @Composable
 fun FistPage(
     composeNavigationViewModel: ComposeNavigationViewModel,
     mainActivityViewModel: MainActivityViewModel,
     inAppPurchaseViewModel: InAppPurchaseViewModel
 ) {
-    when (composeNavigationViewModel.selectedTabItem) {
-        0,1,2 -> {
-            //val scroll = rememberScrollState()
-            Column(
-                Modifier
-                    .background(Color.White)
-            ) {
-                if (mainActivityViewModel.userData.userState == UserState.SIGNED_IN){
-                    UserInfoSection(composeNavigationViewModel, mainActivityViewModel)
-                } else {
-                    Log.d("dwd","Logout state")
-                    UserNameInputSection()
+    when {
+        mainActivityViewModel.isLoading() -> LoadingView()
+        else -> {
+
+            //todo move to another function
+            when (composeNavigationViewModel.selectedTabItem) {
+                0, 1, 2 -> {
+                    //val scroll = rememberScrollState()
+                    Column(
+                        Modifier
+                            .background(Color.White)
+                    ) {
+                        if (mainActivityViewModel.userData.userState == UserState.SIGNED_IN) {
+                            UserInfoSection(composeNavigationViewModel, mainActivityViewModel)
+                        } else {
+                            Log.d("dwd", "Logout state")
+                            UserNameInputSection()
+                        }
+                        Divider(thickness = 1.dp, color = Color(0xFFD9DDE1))
+                        Spacer(modifier = Modifier.height(16.dp))
+                        ImagesContainer(mainActivityViewModel)
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Divider(thickness = 1.dp, color = Color(0xFFD9DDE1))
+                        when (composeNavigationViewModel.selectedTabItem) {
+                            // 0-> LikesSection()
+                            0 -> TabScreenPrice(mainActivityViewModel)
+                            1 -> FollowersSection()
+                            2 -> CommentsSection()
+                        }
+                    }
                 }
-                Divider(thickness = 1.dp, color = Color(0xFFD9DDE1))
-                Spacer(modifier = Modifier.height(16.dp))
-                ImagesContainer(mainActivityViewModel)
-                Spacer(modifier = Modifier.height(20.dp))
-                Divider(thickness = 1.dp, color = Color(0xFFD9DDE1))
-                when(composeNavigationViewModel.selectedTabItem){
-                    0-> LikesSection()
-                    1-> FollowersSection()
-                    2-> CommentsSection()
+
+                3 -> {
+                    Log.d("dwd", "KHJBHN 3")
+                    MorePage(inAppPurchaseViewModel, mainActivityViewModel)
                 }
             }
-        }
 
-        3 -> {
-            Log.d("dwd","KHJBHN 3")
-            MorePage(inAppPurchaseViewModel, mainActivityViewModel)
+
         }
     }
+
 
 }
