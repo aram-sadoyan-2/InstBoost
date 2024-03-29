41697159096d752e10edbe13a7b1001976295773
aramsadoyan <aram.sadoyan@picsart.com>
2024-03-29 15:03:22 +0400

    constants

--- app/src/main/java/com/ins/boostyou/AppConstants.kt
+++ app/src/main/java/com/ins/boostyou/AppConstants.kt
@@ -3,5 +3,6 @@ package com.ins.boostyou
 class AppConstants {
     companion object {
         const val BOOST_YOU_BASE_URL: String = "https://boostyou.convocraftapp.com"
+        const val APP_ID = 936619743392459
     }
 }
